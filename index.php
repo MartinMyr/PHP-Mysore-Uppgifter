65d5c3bbb735bfeb9c26e467e0191607d21a8d55
MartinMyr <martin.myrmarker@gmail.com>
2018-02-25 20:58:52 +0100

    #8 done

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php 
     include 'include/header.php';
-    include 'include/functions.php';
+    include 'tasks/8.php';
 ?>
 
 
 <?php
 
-make_tag("h1", "hej", "blue");
-
-
-
-make_list($array);
-
-print_r(randomNumber(10, 1 ,6))
+write2Dim($array1)
 ?>
 
 
--- tasks/8.php
+++ tasks/8.php
@@ -1,2 +1,19 @@
 <?php
     /*Skriv en funktion som gör om en tvådimensionell array till en HTML-tabell.*/
+
+
+        $array1 = array(
+            array("Martin", 24),
+            array("Åke", 50),
+            array("Kajsa", 22)
+        );
+
+    function write2Dim($array1){
+    
+        foreach ($array1 as $val) {
+            echo "<li>". $val[0] .": ".$val[1] . "år" . "</li>";
+        }
+        echo "</ul>";
+    }
+
+   
